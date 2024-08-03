ef3c7b648322a49f7ac65c9a655faeef29d9381f
nurzhqn0 <nurzhqn@gmail.com>
2024-08-03 12:05:10 +0500

    Initial Commit

---
+++ Nfactorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Nfactorial
+//
+//  Created by Nurzhan Izimbetov on 03.08.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
